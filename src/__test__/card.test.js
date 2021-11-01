1de0eef5f737363f172a2384bb76615e0d04a7de
Filipe Gutierry <filipegutierry@pop-os.localdomain>
2021-11-01 11:13:17 -0300

    first commit

---
+++ src/__test__/card.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Card from "../component/Card";
+
+test("Should show personal info when the prop isShowPersonalInfo equals true", () => {
+  render(<Card isShowPersonalInfo={true} />);
+  const fromScreen = screen.queryByText("Meu dados pessoais");
+  expect(fromScreen).toBeInTheDocument();
+});
+
+test("Should not show personal info when the prop isShowPersonalInfo equals false or not passed", () => {
+  render(<Card />);
+  const fromScreen = screen.queryByText("Meu dados pessoais");
+  expect(fromScreen).not.toBeInTheDocument();
+});
